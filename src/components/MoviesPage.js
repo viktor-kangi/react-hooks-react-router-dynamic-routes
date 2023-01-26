981dd67bb5caefc5303de95a0b5d831cd135bd94
victor <victor.buthia@student.moringaschool.com>
2023-01-26 09:15:56 +0300

    completed

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,11 +1,28 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
+  // useRouteMatch returns a special object with information about
+  // the currently matched route
+  const match = useRouteMatch()
+
   return (
     <div>
       <MoviesList movies={movies} />
+
+       {/* Adding code to show a message to the user to select a movie if they haven't yet */}
+
+       <Route exact path={match.url}>
+        <h3>Choose a movie from the list above</h3>
+       </Route>
+
+      {/* we can use the current URL from the `match` object as part of the path; this will generate a url like "/movies/:movieId" */}
+
+      <Route path={`${match.url}/:movie_Id`}>
+        <MovieShow movies={movies}/>
+      </Route>
     </div>
   );
 }
